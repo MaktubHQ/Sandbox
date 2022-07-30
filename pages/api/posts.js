4232002ddcaa28527fc4c4f0a900bd81fdfe73cc
elijahola <elijaho@wustl.edu>
2022-07-30 15:52:18 -0500

    500 error

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -1,17 +1,24 @@
 import clientPromise from "../../utils/mongodb";
 
 export default async function handler(req, res) {
+
   const client = await clientPromise;
   const db = client.db("dejobs");
   switch (req.method) {
     case "POST":
-      let bodyObject = JSON.parse(JSON.stringify(req.body));
-      let newPost = await db.collection("joblist").insertOne(bodyObject);
-
-
-      res.redirect(307, '/dejobs')
-      res.redirect(500, '/dejobs')
-      res.json(newPost);
+      try{
+        let bodyObject = JSON.parse(JSON.stringify(req.body));
+        let newPost = await db.collection("joblist").insertOne(bodyObject);
+  
+  
+        res.redirect(307, '/dejobs')
+        res.json(newPost);
+      }
+     
+      catch(err){
+        res.redirect(500, '/dejobs')
+      }
+     
 
       break;
     case "GET":
