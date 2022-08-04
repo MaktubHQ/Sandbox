4bc5b6ab18da7398da51296ef446b3392612df17
elijahola <elijaho@wustl.edu>
2022-08-04 18:20:17 -0500

    try again

--- pages/api/profileapplications.js
+++ pages/api/profileapplications.js
@@ -1,3 +1,4 @@
+import { Router } from "next/router";
 import clientPromise from "../../utils/mongodb";
 
 export default async function handler(req, res) {
@@ -5,17 +6,19 @@ export default async function handler(req, res) {
   const db = client.db("dejobs");
   switch (req.method) {
     case "POST":
-      try{
+      //try{
+    
       let bodyObject = JSON.parse(JSON.stringify(req.body));
       let newPost = await db.collection("applications").insertOne(bodyObject);
-      res.status(200).json(newPost);
-      if(res.body.acknowledged == true){
-        res.redirect([200, 307], '/dejobs')
-      }
-      }
-      catch{
-        res.status(500).json({ error: 'failed to load data' })
-      }
+      res.status(200).json(newPost).redirect([200], '/dejobs')
+     
+      // if(res.body.acknowledged == true){
+      //   res.redirect([200, 307], '/dejobs')
+      // }
+      // }
+      // catch{
+      //   res.status(500).json({ error: 'failed to load data' })
+      // }
       break;
   }
 }
