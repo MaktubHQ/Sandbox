2a5c542b18acab76bdc00aed45d8406649c7fcb5
elijahola <elijaho@wustl.edu>
2022-08-05 13:41:08 -0500

    another test

--- pages/api/profileapplications.js
+++ pages/api/profileapplications.js
@@ -10,8 +10,7 @@ export default async function handler(req, res) {
     
       let bodyObject = JSON.parse(JSON.stringify(req.body));
       let newPost = await db.collection("applications").insertOne(bodyObject);
-      res.status(200).json(newPost).redirect([200], '/dejobs')
-     
+      res.json(newPost)
       // if(res.body.acknowledged == true){
       //   res.redirect([200, 307], '/dejobs')
       // }
