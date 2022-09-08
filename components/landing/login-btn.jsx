f59e687ba943bc6a0928dfd4a22ff09b55359cbb
elijahola <elijaho@wustl.edu>
2022-09-07 19:38:48 -0500

    sign-in creating profile test

--- components/landing/login-btn.jsx
+++ components/landing/login-btn.jsx
@@ -1,8 +1,56 @@
 import React from "react"
 import { useSession, signIn, signOut } from "next-auth/react"
 
+const handleSubmit = async (event) => {
+  // Stop the form from submitting and refreshing the page.
+  event.preventDefault()
+
+
+  // Get data from the form.
+  const data = {
+    email: session.user.email,
+
+  }
+
+  // Send the data to the server in JSON format.
+  const JSONdata = JSON.stringify(data)
+
+  // API endpoint where we send form data.
+  const endpoint = '/../../pages/api/profile'
+
+  // Form the request for sending data to the server.
+  const options = {
+    // The method is POST because we are sending data.
+    method: 'POST',
+    // Tell the server we're sending JSON.
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    // Body of the request is the JSON data we created above.
+    body: JSONdata,
+  }
+
+  // Send the form data to our forms API on Vercel and get a response.
+  const response = await fetch(endpoint, options)
+
+  // Get the response data from server as JSON.
+  // If server returns the name submitted, that means the form works.
+  const result = await response.json()
+  console.log(result)
+  console.log(data)
+}
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json())
+
+const test = () =>{
+handleSubmit()
+  signIn()
+}
+
 const Login = () => {
   // eslint-disable-next-line react-hooks/rules-of-hooks
+  
   const { data: session } = useSession()
   if (session) {
     return (
@@ -16,7 +64,7 @@ const Login = () => {
     return (
       <>
         Not signed in <br />
-        <button className="button" onClick={() => signIn()}>Sign in</button>
+        <button className="button" onClick={() => test()}>Sign in</button>
       </>
     )
   }
