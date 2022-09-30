55f3e74af8056d2e0d944e5a0b3c5b19c70a60f3
elijahola <elijaho@wustl.edu>
2022-09-30 12:13:05 -0500

    fix

--- pages/masterclass/[id].js
+++ pages/masterclass/[id].js
@@ -1,19 +1,19 @@
-import Navbars from "../../components/accessories/navbar";
-import Footer from "../../components/accessories/footer";
+// import Navbars from "../../components/accessories/navbar";
+// import Footer from "../../components/accessories/footer";
 
-export default function Post() {
-  return (
-      <div>
-      <Navbars/>
-      <Footer/>
-      </div>
-  );
-}
+// export default function Post() {
+//   return (
+//       <div>
+//       <Navbars/>
+//       <Footer/>
+//       </div>
+//   );
+// }
 
-export async function getStaticPaths() {
-    // Return a list of possible value for id
-  }
+// export async function getStaticPaths() {
+//     // Return a list of possible value for id
+//   }
 
-  export async function getStaticProps({ params }) {
-    // Fetch necessary data for the blog post using params.id
-  }
+//   export async function getStaticProps({ params }) {
+//     // Fetch necessary data for the blog post using params.id
+//   }
