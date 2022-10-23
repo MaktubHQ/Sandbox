38d47c232fb828ca32b07c240d91947220713b81
elijahola <elijaho@wustl.edu>
2022-10-23 16:30:32 -0500

    skeleton for masterclassv2

--- components/masterclass/masterclasscomponent.js
+++ components/masterclass/masterclasscomponent.js
@@ -14,19 +14,44 @@ const MasterClasses = () => {
 
   {/* This function fetches data from DB. */}
   const { data, error } = useSWR('/api/education', fetcher)
+  //const courses = data.data
 
   {/* This function checks if a user data is received from DB to properly display state. */}
   if (error) return <div>Failed to load</div>
-if (!data) return <div>Magic is loading...</div>
+  if (!data) return <div>Magic is loading...</div>
 
 
     return (
       <div>
+        <br></br>
       <h5 className="edutitle">Welcome to Masterclass!</h5>
       <div className="Tabs">
         {/* This function uses React Cards and fills with DB data. */}
-      <Row xs={1} md={2} className="g-4">
-      {Array.from({ length: 4 }).map((_, idx) => (
+
+        {console.log(data)}
+      <h6 className="hottestcourses">Our hottest courses!</h6>
+      <Row xs={1} md={3} className="g-4">
+      {Array.from({ length: data.data.length-1 }).map((_, idx) => (
+        // eslint-disable-next-line react/jsx-key
+        <Col>
+          <Card onClick={event => {openLink(data.data[idx].link)}}>
+            <Card.Body>
+              <Card.Img variant="top" src={`${idx}.png`}/>
+              <Card.Title>{data.data[idx].name}</Card.Title>
+              {/* <Card.Text>
+                {data.data[idx].description}
+              </Card.Text> */}
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+
+    <br></br>
+
+    <h6 className="hottestcourses">Amazing Resources!</h6>
+      <Row xs={1} md={4} className="g-4">
+      {Array.from({ length: data.data.length }).map((_, idx) => (
         // eslint-disable-next-line react/jsx-key
         <Col>
           <Card onClick={event => {openLink(data.data[idx].link)}}>
